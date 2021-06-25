94fab9877376d33f9b596babf9f156d1f996edce
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-06-25 16:29:33 -0500

    😃Another line added

--- SQLServer/CesarsFridayTrainings/1-Query.sql
+++ SQLServer/CesarsFridayTrainings/1-Query.sql
@@ -9,3 +9,9 @@ SELECT * FROM HR.Employees;
 
 -- Bring the id and the Full name from the table HR.Employees
 SELECT empid, lastname + ' ' + firstname as fullName FROM HR.Employees;
+
+-- The line below doesn't work
+SELECT custid, orderid FROM Sales.Orders GROUP BY orderid;
+-- The line below does work
+SELECT custid, orderid FROM Sales.Orders;
+
