28cfebccf33fea330907eccc819343d267df998f
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-06-25 16:06:01 -0500

    More Scripts added

--- SQLServer/CesarsCapacitation/1-Query.sql
+++ SQLServer/CesarsCapacitation/1-Query.sql
@@ -1,1 +1,11 @@
 SELECT name FROM master.dbo.sysdatabases
+
+-- Uses TSQL2019 database
+USE TSQL2019;
+GO
+
+-- Bring all the data from the table HR.Employees
+SELECT * FROM HR.Employees;
+
+-- Bring the id and the Full name from the table HR.Employees
+SELECT empid, lastname + ' ' + firstname as fullName FROM HR.Employees;
